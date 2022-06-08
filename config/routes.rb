bda2ed70d32f9afad733df689ab737a32a8efb4d
RyutaMatsuoka <105757895+uriryu@users.noreply.github.com>
2022-06-08 09:16:40 +0000

    [added]devise usertable

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "homes#top"
   # root to:はサイトのルートページを決める記述(/にアクセスした場合)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
