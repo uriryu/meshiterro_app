7d01de6ea0470445be6e368e30c74855c5c7edfc
RyutaMatsuoka <105757895+uriryu@users.noreply.github.com>
2022-06-15 23:39:45 +0000

    [finish] meshiterro

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -14,7 +14,8 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
+    # kaminariをインストールしたことで、ページメソッドが使用可能に。
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     
-    @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
     #@post_image.userは特定の投稿画像(@post_image)に紐づいたユーザーの情報を取得。投稿画像に対してユーザは1人なので、取得データは一つ.
     #@user.post_imagesはユーザーが投稿した投稿・画像すべてを取得.
   end
